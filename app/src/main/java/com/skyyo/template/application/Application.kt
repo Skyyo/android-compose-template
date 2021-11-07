653de408ae45321ad2122154549cb4fd5e920600
Denis Rudenko <swifttropicbear@gmail.com>
2021-11-07 13:42:29 +0200

    bump Venom to support Android 12

--- app/src/main/java/com/skyyo/template/application/Application.kt
+++ app/src/main/java/com/skyyo/template/application/Application.kt
@@ -2,6 +2,7 @@ package com.skyyo.template.application
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.github.venom.Venom
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,8 +11,9 @@ class Application : Application() {
     override fun onCreate() {
         super.onCreate()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-//        Venom.createInstance(this).apply {
-//            initialize()
-//            start()}
+        Venom.createInstance(this).apply {
+            initialize()
+            start()
+        }
     }
 }
